d417c91d8399a0e025337da917c64324addfaa06
Sven <sven.marien01@ap.be>
2019-11-13 21:18:05 +0100

    First version with Bulma demo home page

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('navBurger', null) navBurger: ElementRef;
+  @ViewChild('navMenu', null) navMenu: ElementRef;
+  
+  title = 'Bulmademo';
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
+
+}
